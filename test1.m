54d4fd0a287de0d920cd2e2ebe91550e9757a5c1
BrianW4110 <brianwong.bkw@gmail.com>
2024-05-18 22:12:14 -0400

    low_freq

--- test1.m
+++ test1.m
@@ -1,4 +1,6 @@
 %% Takes an audiofile of a song and determines its danceability based on beat strength, overall tempo, and tempo stability
+% danceability is how well a song can be danced to throughout the whole
+% duration
 [x,Fs] = audioread("Daft Punk - One More Time.wav");
 x = mean(x,2); % convert from stereo to mono
 duration = length(x) / Fs;
@@ -10,6 +12,27 @@ overlap = N /2; % 50% overlap
 nfft = 2^nextpow2(1024);
 [S,F,T] = spectrogram(x, N, overlap, nfft, Fs);
 
-%%
+%% display spectrogram
 figure(1);
 spectrogram(x, N, overlap, nfft, Fs, 'yaxis');
+title('spectrogram of unmodified audio')
+
+
+%% finding beat strength
+% beat strength is intensity of beats throughout a song
+
+% 1. locating beats
+% spectrogram of only low frequencies in order to distinguish beats 
+% which usually consists of drum and bass (the sound you would tap your
+% foot or nod your head to)
+
+low_freq = F < 150;
+low_freq_energy = sum(ab(S(low_freq, :)), 1);
+% isolated beats 
+[beats, loc] = findpeaks(low_freq_energy, "MinPeakDistance", Fs/2);
+
+
+
+
+figure(2);
+
